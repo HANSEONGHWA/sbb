ac8c4abc76e255db91c2ae894d237536610e6e3e
HANSEONGHWA <ghk9206@hanmail.net>
2023-05-11 21:39:06 +0900

    질문, 답변 등록 Spring Boot Validation

--- src/main/java/com/mysite/sbb/AnswerController.java
+++ src/main/java/com/mysite/sbb/AnswerController.java
@@ -2,9 +2,11 @@ package com.mysite.sbb;
 
 import com.mysite.sbb.entity.Answer;
 import com.mysite.sbb.entity.Question;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -18,14 +20,19 @@ public class AnswerController {
      * 답변 등록
      * @param model
      * @param id
-     * @param content
+     * @param answerForm
+     * @param bindingResult
      * @return
      */
     @PostMapping("/create/{id}")
     public String createAnswer
-            (Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+            (Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if (bindingResult.hasErrors()){
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
